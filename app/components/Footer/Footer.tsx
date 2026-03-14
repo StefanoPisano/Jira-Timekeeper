cd32a73aa7e545e9c37925b7106cf3fdb4d644cf
Stefano Pisano <s.pisano@keywordsstudios.com>
2026-03-14 18:17:30 +0100

    feat: updated license label

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className={"absolute bottom-0 left-[20px]"}>
                 <div className={"mt-2.5 font-mono text-xs text-center"}>
                     Developed with  by <a className={"underline"} href={"https://stefanopisano.github.io/"}>Stefano Pisano</a><br/>
-                    Source code is under <a className={"underline"} href={"../LICENSE"}>MIT License</a><br/>
+                    Source code is under <a className={"underline"} href={"../LICENSE"}>Apache License 2.0.</a><br/>
                     © {new Date().getFullYear()}
                 </div>
             </div>
