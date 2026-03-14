7a42f8a20ce31d130a025302c7936f12a3091111
Stefano Pisano <s.pisano@keywordsstudios.com>
2026-03-14 18:21:57 +0100

    fix: footer ui

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
 
     return (
         <>
-            <div className={"absolute bottom-0 left-[20px]"}>
+            <div className={"absolute bottom-0 left-[40px]"}>
                 <div className={"mt-2.5 font-mono text-xs text-center"}>
-                    Developed with  by <a className={"underline"} href={"https://stefanopisano.github.io/"}>Stefano Pisano</a><br/>
-                    Source code is under <a className={"underline"} href={"../LICENSE"}>Apache License 2.0.</a><br/>
+                    Developed by <a className={"underline"} href={"https://stefanopisano.github.io/"}>Stefano Pisano</a><br/>
+                    Source code is under<br/><a className={"underline"} href={"../LICENSE"}>Apache License 2.0.</a><br/>
                     © {new Date().getFullYear()}
                 </div>
             </div>
