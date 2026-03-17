8f73125375012fe632e69f3826c2544fef1e4ca3
Stefano Pisano <spisano@keywordsstudios.com>
2026-03-17 14:09:15 +0100

    feat: get version from footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,15 +1,15 @@
+import packageJson from '../../../package.json';
+
 const Footer = () => {
 
     return (
-        <>
-            <div className={"mt-2.5 mb-2.5 font-mono text-xs text-center"}>
-                Developed by <a className={"underline"} href={"https://stefanopisano.github.io/"}>Stefano
-                Pisano</a><br/>
-                Source code is under <a className={"underline"} href={"../LICENSE"}>MIT License.</a><br/>
-                © {new Date().getFullYear()}<br/>
-                <strong>Version 1.0.0</strong>
-            </div>
-        </>
+        <div className={"mt-2.5 mb-2.5 font-mono text-xs text-center"}>
+            Developed by <a className={"underline"} href={"https://stefanopisano.github.io/"}>Stefano
+                Pisano</a><br />
+            Source code is under <a className={"underline"} href={"../LICENSE"}>MIT License.</a><br />
+            © {new Date().getFullYear()}<br />
+            <strong>Version {packageJson.version}</strong>
+        </div>
     )
 }
 
